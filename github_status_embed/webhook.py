00022a1ce8c42918acaf87aba6188f81c82a17dc
Cade Gutscher <65359505+itsokthen@users.noreply.github.com>
2021-10-10 01:18:33 -0400

    Update webhook.py

--- github_status_embed/webhook.py
+++ github_status_embed/webhook.py
@@ -131,7 +131,7 @@ def get_payload(workflow: types.Workflow) -> types.WebhookPayload:
         ),
         types.EmbedField(
             name="Repository",
-            value=f"[{workflow.repository} #{workflow.number}]({REPOSITORY_URL})",
+            value=f"[{workflow.repository} #{workflow.number}]({workflow.repository.url})",
             inline=True,
         ),
         types.EmbedField(
